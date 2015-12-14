4b54f70b7e38073f5c5618c34f105dff182cc42a
kendodragon <kendo_dragon@yahoo.com>
2015-12-14 17:19:26 -0500

    Restored unique Auth Attribute in library. Fixed resource server configs.

---
+++ Libraries/OAuth_Library/GatekeeperAuthorize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace OAuth_Library
+{
+    // Custom Auth Attribute that simply verifies whether a claim is present.
+    // In my code this attribute checks against an entry in the database as well.
+    // I have renamed it and stripped out my code so that someone can see how
+    //   easy it is to create their own Auth Attribute and put it in this library.
+    public class GatekeeperAuthorize : AuthorizationFilterAttribute
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+
+        public override Task OnAuthorizationAsync(HttpActionContext actionContext, System.Threading.CancellationToken cancellationToken)
+        {
+            var principal = actionContext.RequestContext.Principal as ClaimsPrincipal;
+            if (!principal.Identity.IsAuthenticated)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            if (!(principal.HasClaim(x => x.Type == ClaimType && x.Value == ClaimValue)))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            //User is Authorized, complete execution
+            return Task.FromResult<object>(null);
+        }
+    }
+}
